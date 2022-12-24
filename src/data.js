9e0e6933b3859e2a2c399697aad797502dd6e4ee
bayuik <bayuindrakusuma05@gmail.com>
2022-12-25 06:10:03 +0700

    add contact component

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: "Kucing Orange Tidur",
+            tag: "orangeCat",
+            imageUrl: "/images/kucing2.jpg",
+        },
+        {
+            id: 2,
+            name: "Kucing Orange",
+            tag: "kochengOyen",
+            imageUrl: "/images/kucing1.jpg",
+        },
+        {
+            id: 3,
+            name: "Kucing Putih",
+            tag: "whiteCat",
+            imageUrl: "/images/kucing3.jpg",
+        },
+    ];
+};
+
+export { getData };
